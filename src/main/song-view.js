265ee5e03fd13c8bbc6fb4659e3e36cc9a6a65fb
haveyouwantto <haveyouwantto@gmail.com>
2023-09-01 00:25:17 +0800

    add clean lyrics

--- src/main/song-view.js
+++ src/main/song-view.js
@@ -54,25 +54,56 @@ export function setSongInfo(song) {
 export function setLyrics(lyrics) {
     songViewLyrics.innerHTML = '';
     if (lyrics) {
+
         let ord = 0;
-        for (const line of lyrics.lyrics) {
+        const cleaned = cleanLyrics(lyrics.lyrics);
+        console.log(cleaned)
+        for (const line of cleaned) {
             const lineDiv = document.createElement('span')
             lineDiv.classList.add('lyrics')
             lineDiv.id = 'lyrics-' + ord;
             lineDiv.innerText = line.text;
             songViewLyrics.append(lineDiv)
             line.ord = ord++;
+
+            if(line.text.endsWith('\n')) songViewLyrics.append(document.createElement('br'))
         }
-        lrc.load(lyrics.lyrics);
+        lrc.load(cleaned);
     } else {
         lrc.clear();
-        
+
         const lineDiv = document.createElement('span')
         lineDiv.classList.add('lyrics')
         lineDiv.innerText = 'Lyrics not found';
         songViewLyrics.append(lineDiv)
     }
 
+}
+
+function cleanLyrics(lyrics) {
+    // Sort the lyrics by time
+    lyrics.sort((a, b) => a.time - b.time);
+
+    // Merge lyrics with the same time and replace " / " with newline
+    const cleanedLyrics = [];
+    let currentLyric = null;
+
+    for (const lyricObj of lyrics) {
+        if (currentLyric === null) {
+            currentLyric = { ...lyricObj, text: lyricObj.text.replace(" / ", "\n") };
+        } else if (currentLyric.time === lyricObj.time) {
+            currentLyric.text += lyricObj.text.replace(" / ", "\n");
+        } else {
+            cleanedLyrics.push(currentLyric);
+            currentLyric = { ...lyricObj, text: lyricObj.text.replace(" / ", "\n") };
+        }
+    }
+
+    if (currentLyric !== null) {
+        cleanedLyrics.push(currentLyric);
+    }
+
+    return cleanedLyrics;
 }
 
 export function updateLyrics(time) {
